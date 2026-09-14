1c1a77af85825b8155626fc89c5d9adac3951eb4
jtellearroyo <jtellearroyo@gmail.com>
2026-09-14 14:59:47 +0200

    Actualizar next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  images: {
-    domains: ['localhost', 'vercel.app'],
-  },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
