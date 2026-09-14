fb2eadb41867a672e255ff905a03023dde5605ac
jtellearroyo <jtellearroyo@gmail.com>
2026-09-14 14:39:09 +0200

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './app/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        slate: {
+          950: '#030712',
+          900: '#0f172a',
+          800: '#1e293b',
+          700: '#334155',
+        }
+      }
+    },
+  },
+  plugins: [],
+}
